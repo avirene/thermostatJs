e5d8a20e3e74180e4977015fbf3e3c824c359a01
Student <student@makerss-air.mynet>
2020-07-09 14:15:19 +0100

    Added the html file and jQuery

---
+++ src/interface.js
@@ -0,0 +1,37 @@
+"use strict";
+
+$(document).ready(function() {
+  var thermostat = new Thermostat();
+  updateTemperature();
+
+  $("#temperature-up").click(function() {
+    thermostat.up();
+    updateTemperature();
+  });
+
+  $("#temperature-down").click(function() {
+    thermostat.down();
+    updateTemperature();
+  });
+  
+  $("#temperature-reset").click(function() {
+    thermostat.resetTemperature();
+    updateTemperature();
+  });
+
+  $("#powersaving-on").click(function() {
+    thermostat.turnPowerSavingModeOn();
+    $("#power-saving-status").text("on")
+    updateTemperature();
+  });
+
+  $("#powersaving-off").click(function() {
+    thermostat.turnPowerSavingModeOff();
+    $("#power-saving-status").text("off")
+    updateTemperature();
+  });
+
+  function updateTemperature() {
+    $("#temperature").text(thermostat.temperature);
+  };
+});
