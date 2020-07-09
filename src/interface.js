4fa7c09bdbdb48490917acee66ca30b3fabd223b
Student <student@makerss-air.mynet>
2020-07-09 17:04:54 +0100

    Added the AJAX current weather functionality

--- src/interface.js
+++ src/interface.js
@@ -1,6 +1,15 @@
 "use strict";
 
 $(document).ready(function() {
+
+  displayWeather("London");
+
+  $("#select-city").submit(function(event) {
+    event.preventDefault();
+    var city = $("#current-city").val();
+    displayWeather(city);
+  })
+
   var thermostat = new Thermostat();
   updateTemperature();
 
@@ -35,4 +44,14 @@ $(document).ready(function() {
     $("#temperature").text(thermostat.temperature);
     $("#temperature").attr("class", thermostat.energyUsage());
   };
+
+  function displayWeather(city) {
+    var url = "http://api.openweathermap.org/data/2.5/weather?q=" + city;
+    var token = "&appid=a3d9eb01d4de82b9b8d0849ef604dbed";
+    var units = "&units=metric";
+    $.get(url + token + units, function(data) {
+      $("#current-temperature").text(data.main.temp);
+    })
+  }
+
 });
